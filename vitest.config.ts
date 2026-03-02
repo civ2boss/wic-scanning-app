9aa99f010b2344d5c49ed412c00efe265666e367
Robin Ting <civ2boss@gmail.com>
2026-03-02 16:42:54 -0500

    test: Setup React Testing Library and Vitest for frontend

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    environment: 'node',
-    include: ['src/**/*.test.ts'],
+    environment: 'jsdom',
+    include: ['src/**/*.test.ts', 'src/**/*.test.tsx'],
+    setupFiles: ['./src/setupTests.ts'],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
